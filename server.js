7358f6ea15cc3d030d617fa4d8d309ccfcdd9e1a
Julian Hansby <jhansby@mhg.co.za>
2018-04-13 15:17:03 +0200

    first file commit for this proj

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+ 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// static / public dir
+app.use(express.static(path.join(__dirname, '/public')));
+
+// route to index file
+app.get('/', function(){
+	res.sendFile(path.join(__dirname,'/public/index.html'))
+})
+
+app.listen(8080, function () {
+    console.log('Node app is running on port 8080');
+});
+
+module.exports = app;
